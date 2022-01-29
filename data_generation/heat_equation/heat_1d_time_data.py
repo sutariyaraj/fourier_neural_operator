7d7b0f454990439e653ddeaedafa6858985c65c2
Raj <sutariyaraj77725@gmail.com>
2022-01-29 15:37:59 +0100

    equation fixed

--- data_generation/heat_equation/heat_1d_time_data.py
+++ data_generation/heat_equation/heat_1d_time_data.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 import random
 
 
-def generate_heat_equation_data(amplitude=1.0, c=1, phase=0):
+def generate_heat_equation_data(amplitude=1.0, n=1, phase=0):
     """
     @param amplitude: maximum temperature of initial condition
-    @param c: [int] no of harmonics of wave of initial condition
+    @param n: [int] no of harmonics of wave of initial condition
     @param phase: phase shift of the initial condition
     @return: numpy array
     """
 
     L=1             # length of rod in cm
     max_t = 2       # maximum time steps in seconds
-    alpha = 0.03       # intrinsic property of metal used - fixed for same type of metal
-    exponential_const = (c*np.pi/L)**2
+    c = 0.03       # intrinsic property of metal used - fixed for same type of metal
+    exponential_const = (c * n * np.pi / L) ** 2
 
     t = np.linspace(0, max_t, 200)
     x = np.linspace(0, L, 200)
     X, T = np.meshgrid(x, t)
     # X = X.reshape(-1,1)
     # T = T.reshape(-1,1)
-    U = amplitude*(np.exp(-alpha*exponential_const*T))*np.sin((c*np.pi*X/L) + phase)
+    U = amplitude*(np.exp(-exponential_const*T))*np.sin((n * np.pi * X / L) + phase)
     U = U.reshape(200,200)
 
     # [DEBUG]
@@ -51,7 +51,7 @@ def generate_dataset():
         for harmonics in range(1,11):
             for phase_shift in range(10):
                 phase_shift = random.random() * 2 * np.pi
-                heat_dataset[cnt] = generate_heat_equation_data(amplitude=random.random(), c=harmonics, phase=phase_shift)
+                heat_dataset[cnt] = generate_heat_equation_data(amplitude=random.random(), n=harmonics, phase=phase_shift)
                 cnt += 1
     np.random.shuffle(heat_dataset)
     return heat_dataset
